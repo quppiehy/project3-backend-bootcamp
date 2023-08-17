56ca16fe001e911fcae199ed66d44508df4cabdd
quppiehy <127730321+quppiehy@users.noreply.github.com>
2023-08-17 12:35:09 +0800

    Revert "Added seller part"

--- db/models/chat.js
+++ db/models/chat.js
@@ -3,12 +3,8 @@ const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Chat extends Model {
     static associate(models) {
-      this.belongsTo(models.product, {
-        foreignKey: "productId",
-        // as: "productChat",
-      });
+      this.belongsTo(models.product, { foreignKey: "productId" });
       this.belongsTo(models.user, { foreignKey: "userId" });
-      this.belongsTo(models.user, { foreignKey: "sellerId" });
       this.hasMany(models.chat_message);
     }
   }
@@ -20,27 +16,8 @@ module.exports = (sequelize, DataTypes) => {
         unique: true,
         autoIncrement: true,
       },
-      productId: {
-        type: DataTypes.INTEGER,
-        references: {
-          model: "product",
-          key: "id",
-        },
-      },
-      userId: {
-        type: DataTypes.INTEGER,
-        references: {
-          model: "user",
-          key: "id",
-        },
-      },
-      sellerId: {
-        type: DataTypes.INTEGER,
-        references: {
-          model: "user",
-          key: "id",
-        },
-      },
+      productId: DataTypes.INTEGER,
+      userId: DataTypes.INTEGER,
     },
     {
       sequelize,
